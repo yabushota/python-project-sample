904d604f5df4aba0a8b94c4e39ed73c1e5802d3a
yabushota <e1623110@oit.ac.jp>
2025-05-13 18:37:36 +0900

    Update sin_wave.py

--- sin_wave.py
+++ sin_wave.py
@@ -1,1 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# 0から2πまでを100等分したデータを作成
+x = np.linspace(0, 2 * np.pi, 100)
+
+# xのサイン値を計算
+y = np.sin(x)
+
+# グラフの作成
+plt.plot(x, y)
+plt.title("Sine Wave")
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+
+# グラフを画面に表示
+plt.show()
